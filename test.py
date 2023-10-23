27c4cb577afe6b47d2991631ccc4ca4ffd5817b2
Juan Martin Zabala Ochoa <juanmarzabala8a@gmail.com>
2023-10-23 10:41:02 -0500

    Initial commit (the sequel)

---
+++ test.py
@@ -0,0 +1,46 @@
+from pickle import load
+from numpy import argmax
+from keras.preprocessing.sequence import pad_sequences
+from keras.applications.xception import Xception, preprocess_input
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.models import Model
+from keras.models import load_model
+
+def extract_features(filename):
+	model = Xception()
+	model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
+	image = load_img(filename, target_size=(299, 299))
+	image = img_to_array(image)
+	image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+	image = preprocess_input(image)
+	feature = model.predict(image, verbose=0)
+	return feature
+
+def word_for_id(integer, tokenizer):
+	for word, index in tokenizer.word_index.items():
+		if index == integer:
+			return word
+	return None
+
+def generate_desc(model, tokenizer, photo, max_length):
+	in_text = 'start'
+	for _ in range(max_length):
+		sequence = tokenizer.texts_to_sequences([in_text])[0]
+		sequence = pad_sequences([sequence], maxlen=max_length)
+		yhat = model.predict([photo,sequence], verbose=0)
+		yhat = argmax(yhat)
+		word = word_for_id(yhat, tokenizer)
+		if word is None:
+			break
+		in_text += ' ' + word
+		if word == 'end':
+			break
+	return in_text
+
+tokenizer = load(open('tokenizer.pkl', 'rb'))
+max_length = 34
+model = load_model('checkpoint/model-ep003-loss3.391-val_loss3.755.h5')
+photo = extract_features('Flicker8k_Dataset/667626_18933d713e.jpg')
+description = generate_desc(model, tokenizer, photo, max_length)
+print(description)
